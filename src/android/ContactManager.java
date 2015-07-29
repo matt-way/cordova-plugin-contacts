9a8ad806192c850647404f5375453bde6ade5a62
Matt Way <matt__way@hotmail.com>
2015-07-30 09:32:30 +1000

    android pick testing

--- src/android/ContactManager.java
+++ src/android/ContactManager.java
@@ -160,8 +160,11 @@ public class ContactManager extends CordovaPlugin {
      * @throws JSONException
      */
     public void onActivityResult(int requestCode, int resultCode, final Intent intent) {
+        Log.e(LOG_TAG, "!!! activity result", e);
         if (requestCode == CONTACT_PICKER_RESULT) {
+            Log.e(LOG_TAG, "!!! contact picker result", e);
             if (resultCode == Activity.RESULT_OK) {
+                Log.e(LOG_TAG, "!!! picker result OK", e);
                 String contactId = intent.getData().getLastPathSegment();
                 // to populate contact data we require  Raw Contact ID
                 // so we do look up for contact raw id first
@@ -182,9 +185,11 @@ public class ContactManager extends CordovaPlugin {
                     Log.e(LOG_TAG, "JSON fail.", e);
                 }
             } else if (resultCode == Activity.RESULT_CANCELED){
+                Log.e(LOG_TAG, "!!! activity cancelled", e);
                 this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.NO_RESULT, UNKNOWN_ERROR));
                 return;
             }
+            Log.e(LOG_TAG, "!!! no result in if", e);
             this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, UNKNOWN_ERROR));
         }
     }
